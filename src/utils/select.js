c5faef67fa8cdceb8dc5c85a9aab2940de229858
Marc Backes <hello@marc.dev>
2021-12-04 10:51:51 +0100

    initial commit 📦

---
+++ src/utils/select.js
@@ -0,0 +1,7 @@
+import boxes from '../data/boxes.js'
+
+export const selectBox = item => {
+    // Select the smallest possible box from `boxes` for any given item
+    // An item looks something like {width: X, length: Y, height: Z}
+    return undefined
+}
